fd9f75978c3c079752ed71fbb0443505e84f4364
cer-ches3 <114877440+cer-ches3@users.noreply.github.com>
2023-05-20 18:23:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('certimg1').onclick = function(){
+    document.getElementById('cert1').style.display = 'block'
+}
+document.getElementById('close1').onclick = function(){
+    document.getElementById('cert1').style.display = 'none'
+}
+
+document.getElementById('certimg2').onclick = function(){
+    document.getElementById('cert2').style.display = 'block'
+}
+document.getElementById('close2').onclick = function(){
+    document.getElementById('cert2').style.display = 'none'
+}
