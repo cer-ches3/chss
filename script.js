a0740def47bb24af83e1d172531c38c36a1d1cf5
cer-ches3 <114877440+cer-ches3@users.noreply.github.com>
2023-05-20 18:38:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ document.getElementById('certimg2').onclick = function(){
 document.getElementById('close2').onclick = function(){
     document.getElementById('cert2').style.display = 'none'
 }
+
+
+document.getElementById('m-about').onclick=function(){
+    document.getElementById('about').scrollIntoView({behavior: "smooth"})
+  }
+  document.getElementById('m-education').onclick=function(){
+    document.getElementById('education').scrollIntoView({behavior: "smooth"})
+  }
+  document.getElementById('m-work').onclick=function(){
+    document.getElementById('work').scrollIntoView({behavior: "smooth"})
+  }
+  document.getElementById('m-skill').onclick=function(){
+    document.getElementById('skill').scrollIntoView({behavior: "smooth"})
+  }
+  document.getElementById('m-myWork').onclick=function(){
+    document.getElementById('myWork').scrollIntoView({behavior: "smooth"})
+  }
